1b9ef260f18643a90fbb3d886415be1bec248868
JWWon <wonjiwn@naver.com>
2017-03-10 13:59:48 +0900

    change Daum api settings

--- app/src/main/java/com/jiwoon/tgwing/mapsns/MainActivity.java
+++ app/src/main/java/com/jiwoon/tgwing/mapsns/MainActivity.java
@@ -4,26 +4,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ViewGroup;
 
-import net.daum.mf.map.api.MapPoint;
 import net.daum.mf.map.api.MapView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String API_KEY = "0dcd3d83bc6dc4d32f06a63b3119e6d0";
+    private static final String API_KEY = "7d21acf9557f38468561eff63f87ce8e";
+
+    public MapView mMapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        MapView daumMap = new MapView(this);
-        daumMap.setDaumMapApiKey(API_KEY);
+        mMapView = new MapView(MainActivity.this);
+        mMapView.setDaumMapApiKey(API_KEY);
 
         ViewGroup mapContainerView = (ViewGroup) findViewById(R.id.daum_map);
-        mapContainerView.addView(daumMap);
-
-        // 중심점 변경
-        daumMap.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(37.53737528, 127.00557633), true);
+        mapContainerView.addView(mMapView);
+//
+//        // 중심점 변경
+//        daumMap.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(37.53737528, 127.00557633), true);
     }
 }
