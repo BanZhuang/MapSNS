2ff0a40e45f2bf5a766994d619e89adc7c8841fd
JWWon <wonjiwn@naver.com>
2017-03-10 10:02:21 +0900

    add Daum MapView

--- app/src/main/java/com/jiwoon/tgwing/mapsns/MainActivity.java
+++ app/src/main/java/com/jiwoon/tgwing/mapsns/MainActivity.java
@@ -2,6 +2,10 @@ package com.jiwoon.tgwing.mapsns;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewGroup;
+
+import net.daum.mf.map.api.MapPoint;
+import net.daum.mf.map.api.MapView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +16,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        MapView daumMap = new MapView(this);
+        daumMap.setDaumMapApiKey(API_KEY);
+
+        ViewGroup mapContainerView = (ViewGroup) findViewById(R.id.daum_map);
+        mapContainerView.addView(daumMap);
+
+        // 중심점 변경
+        daumMap.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(37.53737528, 127.00557633), true);
     }
 }
